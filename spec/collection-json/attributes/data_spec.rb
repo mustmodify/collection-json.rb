b0d62926882210f2b52b71710eede5aa260fb910
Johnathon Wright <jw@mustmodify.com>
2014-07-16 17:51:37 -0500

    #1 - missing test

--- spec/collection-json/attributes/data_spec.rb
+++ spec/collection-json/attributes/data_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 require 'collection-json/attributes/data'
 
 describe CollectionJSON::Data do
+  it 'accepts "conditions" attribute' do
+    CollectionJSON::Data.from_hash(conditions: [{field: 'description', value: 'true'}]).to_json.should == "{\"conditions\":[{\"field\":\"description\",\"value\":\"true\"}]}"
+  end
+
   it 'displays value "false"' do
     CollectionJSON::Data.from_hash(value: false).to_json.should == "{\"value\":false}"
   end
